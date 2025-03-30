ba625c6e21c9037b121a4395baaa4eeaa862fba4
Daniel Santander <daniel.santander@tapp.cl>
2025-03-30 19:18:07 -0300

    first commit

---
+++ src/main/java/com/duoc/week3d/Week3dApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.week3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week3dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/week3d/Week3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.week3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
