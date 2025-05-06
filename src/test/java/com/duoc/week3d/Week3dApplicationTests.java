f57dab75c1f432ccd1fbd991c9aaaae38572d083
Daniel Santander <daniel.santander@tapp.cl>
2025-05-05 22:27:39 -0400

    fix: remove targe from gitignore and add wallet

--- src/test/java/com/duoc/week3d/Week3dApplicationTests.java
+++ src/test/java/com/duoc/week3d/Week3dApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Week3dApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
